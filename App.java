e6341e245b66521ac4c1b29c48218334137b0e91
Francisco Santana Cardoso <fsantanagy@gmail.com>
2021-10-20 07:28:14 -0300

    Correção nas chamadas estaticas

--- App.java
+++ App.java
@@ -3,29 +3,29 @@ public class App {
         Quadrado calcQuadrado;
         Circulo calcCirculo;
         
+        Quadrado.setDiag(1.41);
         calcQuadrado = new Quadrado(2.5);
-        calcQuadrado.setDiag(1.41);
         System.out.println("---------------------------------------------");
         System.out.printf("Área do quadrado : %.3f cm2 \n", calcQuadrado.getArea());
         System.out.printf("Perimetro do quadrado : %.3f cm \n", calcQuadrado.getPerimetro());
         System.out.printf("Diagonal do quadrado : %.3f cm\n", calcQuadrado.getDiagonal());
         System.out.println("---------------------------------------------");
        
-        calcQuadrado.setDiag(Math.sqrt(2));
+        Quadrado.setDiag(Math.sqrt(2));
         System.out.println("---------------------------------------------");
         System.out.printf("Área do quadrado (Math): %.3f cm2 \n", calcQuadrado.getArea());
         System.out.printf("Perimetro do quadrado (Math): %.3f cm \n", calcQuadrado.getPerimetro());
         System.out.printf("Diagonal do quadrado (Math): %.3f cm\n", calcQuadrado.getDiagonal());
         System.out.println("---------------------------------------------");
         
+        Circulo.setPi(3.14);
         calcCirculo = new Circulo(2.5);
-        calcCirculo.setPi(3.14);
         System.out.println("---------------------------------------------");
         System.out.printf("Área do Circulo : %.3f cm2 \n",calcCirculo.getArea());
-        System.out.printf("Circunferencia do Circulo : %.3f cm2\n",calcCirculo.getCircunferencia());
+        System.out.printf("Circunferencia do Circulo (Math): %.3f cm2\n",calcCirculo.getCircunferencia());
         System.out.println("---------------------------------------------");
     
-        calcCirculo.setPi(Math.PI);
+        Circulo.setPi(Math.PI);
         System.out.println("---------------------------------------------");
         System.out.printf("Área do Circulo (Math): %.3f cm2 \n",calcCirculo.getArea());
         System.out.printf("Circunferencia do Circulo (Math): %.3f cm2\n",calcCirculo.getCircunferencia());
